2dc5e3bda75d690a8cfc802e625c9668a9d81aba
Hunter Sandlin <huntergsandlin@gmail.com>
2026-04-19 11:14:28 -0400

    Created theme file and styles folder

---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,13 @@
+export const hoverTransition = {
+    transition: 'color 0.4s ease',
+    '&:hover': { color: 'rgba(163, 163, 163, 1)' }
+};
+
+export const fontFamily = 'Source Sans Pro, Helvetica Neue, Helvetica, Arial, sans-serif';
+
+export const brandColors = {
+    red: '#b00000',
+    redLight: '#cd0e11',
+    redDark: '#a6090b',
+    gold: '#ffd700'
+};
