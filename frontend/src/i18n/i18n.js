c5ae60f497dbefeb6cb7113f33bd865c6f914b0c
Hunter Sandlin <huntergsandlin@gmail.com>
2026-04-22 19:38:26 -0400

    Added state translations to i18n

--- frontend/src/i18n/i18n.js
+++ frontend/src/i18n/i18n.js
@@ -4,21 +4,25 @@ import { initReactI18next } from 'react-i18next';
 import enNavbar from './locales/en/navbar.json';
 import enCommon from './locales/en/common.json';
 import enFooter from './locales/en/footer.json';
+import states from './locales/en/states.json';
 // ES Imports
 import esNavbar from './locales/es/navbar.json';
 import esCommon from './locales/es/common.json';
 import esFooter from './locales/es/footer.json';
+import esStates from './locales/es/states.json';
 
 const resources = {
     en: {
         common: enCommon,
         navbar: enNavbar,
-        footer: enFooter
+        footer: enFooter,
+        states: states
     },
     es: {
         common: esCommon,
         navbar: esNavbar,
-        footer: esFooter
+        footer: esFooter,
+        states: esStates
     }
 };
 
