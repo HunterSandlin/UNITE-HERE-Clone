5ee189f247413602a3cfdf1b4451dd31df1487f7
Hunter Sandlin <huntergsandlin@gmail.com>
2026-04-24 10:50:55 -0400

    Added endpoints for article views

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import RegisterView, profile
+from .views import RegisterView,NewsArticleListCreateView, NewsArticleDetailView, RecentNewsView, profile
 from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', ObtainAuthToken.as_view(), name='login'),
     path('profile/', profile, name='profile'),
+    path('news/', NewsArticleListCreateView.as_view()),
+    path('news/recent/', RecentNewsView.as_view()),
+    path('news/<int:pk>/', NewsArticleDetailView.as_view()),
 ]
