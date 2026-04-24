39fff22ed96fa31ec83fce62355ad7bc16487d8f
Hunter Sandlin <huntergsandlin@gmail.com>
2026-04-24 10:50:12 -0400

    Created admin register for article model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import NewsArticle
 
 # Register your models here.
+@admin.register(NewsArticle)
+class NewsArticleAdmin(admin.ModelAdmin):
+    list_display  = ['title', 'type', 'featured', 'published']
+    list_filter   = ['type', 'featured']
+    ordering      = ['-published']
