9db7464bab529e293265d766fd6e9b8a49fd3ad8
Hunter Sandlin <huntergsandlin@gmail.com>
2026-04-24 10:49:37 -0400

    Switched DB to postgres, added static files, added django seed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken', 
     'corsheaders',              
     'api',
+    'django_seed',
 ]
 
 MIDDLEWARE = [
@@ -93,8 +94,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'uhclone',
+        'USER': 'uhclone',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -134,3 +139,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / 'media' 
+MEDIA_URL  = '/media/' 
